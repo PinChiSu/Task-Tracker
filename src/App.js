814620c969e5e73ad1ca3aea0c67e0db570d2536
PinChi <sueric68@gmail.com>
2021-03-30 01:38:09 +0800

    completed component props

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App= () => {
+  const name = 'Pinch'
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
