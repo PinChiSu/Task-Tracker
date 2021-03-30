875484ab04de591211fa610f3fa42b08a9aa7c3d
PinChi <sueric68@gmail.com>
2021-03-31 02:35:55 +0800

    Completed icon with react-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App= () => {
-  const name = 'Pinch'
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Mina protocol',
+        day: 'April 13th at 12am',
+        reminder: true,
+
+    },
+    {
+        id: 2,
+        text: 'Boson protocol',
+        day: 'April 7th at 7pm',
+        reminder: true,
+
+    },
+    {
+        id: 3,
+        text: 'AIOZ protocol',
+        day: 'April 2th at 11am',
+        reminder: true,
+
+    },
+
+]
+)
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
